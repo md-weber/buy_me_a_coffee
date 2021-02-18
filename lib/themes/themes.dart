c52492f053d760209ab62306ed675ea936cc94dd
Max Weber <weber.max.1988@live.de>
2021-02-18 19:55:33 +0100

    [Refactoring] added lint package (#10)
    Co-authored-by: Max <flutterexp@gmail.com>

--- lib/themes/themes.dart
+++ lib/themes/themes.dart
@@ -12,62 +12,80 @@ abstract class BuyMeACoffeeThemeData {
 /// [OrangeTheme] is a implementation of [BuyMeACoffeeThemeData]
 /// it has a orange background and a white text color
 class OrangeTheme implements BuyMeACoffeeThemeData {
-  Color backgroundColor = Color(0xFFFF813F);
+  @override
+  Color backgroundColor = const Color(0xFFFF813F);
+  @override
   Color textColor = Colors.white;
 }
 
 /// [YellowTheme] is a implementation of [BuyMeACoffeeThemeData]
 /// it has a yellow background and a black text color
 class YellowTheme implements BuyMeACoffeeThemeData {
-  Color backgroundColor = Color(0xFFFFDD00);
+  @override
+  Color backgroundColor = const Color(0xFFFFDD00);
+  @override
   Color textColor = Colors.black;
 }
 
 /// [PurpleTheme] is a implementation of [BuyMeACoffeeThemeData]
 /// it has a Purple background and a white text color
 class PurpleTheme implements BuyMeACoffeeThemeData {
-  Color backgroundColor = Color(0xFFbd5fff);
+  @override
+  Color backgroundColor = const Color(0xFFbd5fff);
+  @override
   Color textColor = Colors.white;
 }
 
 /// [BlackTheme] is a implementation of [BuyMeACoffeeThemeData]
 /// it has a black background and a white text color
 class BlackTheme implements BuyMeACoffeeThemeData {
+  @override
   Color backgroundColor = Colors.black;
+  @override
   Color textColor = Colors.white;
 }
 
 /// [WhiteTheme] is a implementation of [BuyMeACoffeeThemeData]
 /// it has a white background and a black text color
 class WhiteTheme implements BuyMeACoffeeThemeData {
+  @override
   Color backgroundColor = Colors.white;
+  @override
   Color textColor = Colors.black;
 }
 
 /// [BlueTheme] is a implementation of [BuyMeACoffeeThemeData]
 /// it has a blue background and a white text color
 class BlueTheme implements BuyMeACoffeeThemeData {
-  Color backgroundColor = Color(0xFF5F7FFF);
+  @override
+  Color backgroundColor = const Color(0xFF5F7FFF);
+  @override
   Color textColor = Colors.white;
 }
 
 /// [TealTheme] is a implementation of [BuyMeACoffeeThemeData]
 /// it has a teal background and a white text color
 class TealTheme implements BuyMeACoffeeThemeData {
-  Color backgroundColor = Color(0xFF79D6B5);
+  @override
+  Color backgroundColor = const Color(0xFF79D6B5);
+  @override
   Color textColor = Colors.white;
 }
 
 /// [RedTheme] is a implementation of [BuyMeACoffeeThemeData]
 /// it has a lightRed background and a white text color
 class RedTheme implements BuyMeACoffeeThemeData {
-  Color backgroundColor = Color(0xFFFF5F5F);
+  @override
+  Color backgroundColor = const Color(0xFFFF5F5F);
+  @override
   Color textColor = Colors.white;
 }
 
 /// [PinkTheme] is a implementation of [BuyMeACoffeeThemeData]
 /// it has a pink background and a white text color
 class PinkTheme implements BuyMeACoffeeThemeData {
-  Color backgroundColor = Color(0xFFF471FF);
+  @override
+  Color backgroundColor = const Color(0xFFF471FF);
+  @override
   Color textColor = Colors.white;
 }
