5169724763534ef45a2452c1633f5b4571ab0be0
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2023-06-01 03:43:28 +0300

    rework package

--- lib/buy_me_a_coffee_widget.dart
+++ lib/buy_me_a_coffee_widget.dart
@@ -1,4 +1,3 @@
-library buy_me_a_coffee_widget;
 
-export 'package:buy_me_a_coffee_widget/themes/themes.dart';
 export 'package:buy_me_a_coffee_widget/src/buy_me_a_coffee_widget.dart';
+export 'package:buy_me_a_coffee_widget/themes/themes.dart';
--- lib/src/buy_me_a_coffee_widget.dart
+++ lib/src/buy_me_a_coffee_widget.dart
@@ -2,6 +2,7 @@ import 'package:buy_me_a_coffee_widget/themes/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 /// Url to the buy me a coffee website
 const String buyMeACoffeeUrl = "https://www.buymeacoffee.com/";
@@ -14,17 +15,30 @@ class BuyMeACoffeeWidget extends StatelessWidget {
   const BuyMeACoffeeWidget({
     Key? key,
     required this.sponsorID,
-    this.customText = "Buy me a coffee",
+    String this.customText = "Buy me a coffee",
     this.textStyle,
     this.backgroundColor,
     this.theme,
-  }) : super(key: key);
+  })  : _isIconOnly = false,
+        super(key: key);
+
+  const BuyMeACoffeeWidget.iconOnly({
+    Key? key,
+    required this.sponsorID,
+    this.backgroundColor,
+    this.theme,
+  })  : _isIconOnly = false,
+        customText = null,
+        textStyle = null,
+        super(key: key);
+
+  final bool _isIconOnly;
 
   /// The id for the user where it should link to.
   final String sponsorID;
 
   /// Custom text for the widget
-  final String customText;
+  final String? customText;
 
   /// Overwrites the textStyle of the widget
   final TextStyle? textStyle;
@@ -48,9 +62,9 @@ class BuyMeACoffeeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Color? backgroundColor;
-    TextStyle? textStyle;
-
+    var backgroundColor = this.backgroundColor;
+    final textStyle = this.textStyle;
+    final customText = this.customText;
     if (theme == null && backgroundColor == null) {
       backgroundColor = const Color(0xFFFF813F);
     } else if (backgroundColor != null) {
@@ -58,21 +72,25 @@ class BuyMeACoffeeWidget extends StatelessWidget {
     } else {
       backgroundColor = theme!.backgroundColor;
     }
-
-    return GestureDetector(
-      onTap: () async {
-        final urlString = buyMeACoffeeUrl + sponsorID;
-        if (await canLaunch(urlString)) {
-          await launch(urlString);
-        } else {
-          throw "BuyMeACoffeeWidget - Something went wrong!";
-        }
-      },
-      child: ConstrainedBox(
-        constraints: const BoxConstraints(minWidth: 217.0),
+    final iconWidget = SvgPicture.network(
+      "https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg",
+      width: 35.0,
+    );
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxWidth: _isIconOnly ? 51 : double.infinity,
+        maxHeight: 51,
+      ),
+      child: InkWell(
+        onTap: () {
+          final uri = Uri.parse(buyMeACoffeeUrl + sponsorID);
+          launchUrl(uri);
+        },
         child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 7.0, horizontal: 10.0),
-          height: 51.0,
+          padding: EdgeInsets.symmetric(
+            vertical: 7.0,
+            horizontal: _isIconOnly ? 0 : 10.0,
+          ),
           decoration: BoxDecoration(
             color: backgroundColor,
             border: Border.all(color: Colors.transparent),
@@ -80,35 +98,36 @@ class BuyMeACoffeeWidget extends StatelessWidget {
               BoxShadow(
                 color: const Color.fromRGBO(190, 190, 190, 0.5),
                 blurRadius: 2.0,
-                offset: Offset.lerp(const Offset(0, 0), const Offset(1, 1), 1)!,
+                offset: Offset.lerp(Offset.zero, const Offset(1, 1), 1)!,
               )
             ],
             borderRadius: const BorderRadius.all(
               Radius.circular(5.0),
             ),
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SvgPicture.network(
-                "https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg",
-                width: 35.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: Text(
-                  customText,
-                  style: textStyle ??
-                      TextStyle(
-                        fontFamily: "Cookie",
-                        color: theme == null ? Colors.white : theme!.textColor,
-                        fontSize: 28.0,
-                        letterSpacing: 0.6,
+          child: _isIconOnly || customText == null || customText.isEmpty
+              ? iconWidget
+              : Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    iconWidget,
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text(
+                        customText,
+                        style: textStyle ??
+                            GoogleFonts.cookie(
+                              color: theme == null
+                                  ? Colors.white
+                                  : theme!.textColor,
+                              fontSize: 28.0,
+                              letterSpacing: 0.6,
+                            ),
                       ),
+                    )
+                  ],
                 ),
-              )
-            ],
-          ),
         ),
       ),
     );
